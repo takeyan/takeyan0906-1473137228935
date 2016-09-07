3906ffba8eaf295e36ff9e7051754a9f295a8abd
SEIICHI TAKEUCHI <takeyan@jp.ibm.com>
2016-09-06 21:30:18 -0500

    message changed. Hello World!.

--- src/main/java/wasdev/sample/servlet/SimpleServlet.java
+++ src/main/java/wasdev/sample/servlet/SimpleServlet.java
@@ -21,7 +21,7 @@ public class SimpleServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
-        response.getWriter().print("Hello Takeyan2!");
+        response.getWriter().print("Hello World!");
     }
 
 }
